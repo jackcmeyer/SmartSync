670ead6469e585b7fd9d129ad8dbf68e1c117ddd
KrashLeviathan <nathankarasch@gmail.com>
2017-02-09 15:27:47 -0600

    Finished main features of ss_serve.bash

--- ss_serve.bash
+++ ss_serve.bash
@@ -13,35 +13,42 @@ handler()
     kill -9 $PID3
     echo "kill -9 $PID4"
     kill -9 $PID4
-    echo "kill -9 $PID5"
-    kill -9 $PID5
 }
 
 echo "Starting SmartSync services..."
 
-echo "java -jar service1.jar param param param"
-sleep 60 &
+# Run maven package on services
+echo "Running Maven packages..."
+cd config-service
+mvn package
+cd ../eureka-service
+mvn package
+cd ../hystrix-dashboard
+mvn package
+cd ../zipkin-service
+mvn package
+cd ..
+
+echo "java -jar config-service/target/config-service-0.0.1-SNAPSHOT.jar"
+java -jar config-service/target/config-service-0.0.1-SNAPSHOT.jar &
 PID1=$!
 echo $PID1
+sleep 10
 
-echo "java -jar service2.jar param param param"
-sleep 60 &
+echo "java -jar eureka-service/target/eureka-service-0.0.1-SNAPSHOT.jar"
+java -jar eureka-service/target/eureka-service-0.0.1-SNAPSHOT.jar &
 PID2=$!
 echo $PID2
+sleep 10
 
-echo "java -jar service3.jar param param param"
-sleep 60 &
+echo "java -jar hystrix-dashboard/target/hystrix-dashboard-0.0.1-SNAPSHOT.jar"
+java -jar hystrix-dashboard/target/hystrix-dashboard-0.0.1-SNAPSHOT.jar &
 PID3=$!
 echo $PID3
 
-echo "java -jar service4.jar param param param"
-sleep 60 &
+echo "java -jar zipkin-service/target/zipkin-service-0.0.1-SNAPSHOT.jar"
+java -jar zipkin-service/target/zipkin-service-0.0.1-SNAPSHOT.jar &
 PID4=$!
 echo $PID4
-
-echo "java -jar service5.jar param param param"
-sleep 60 &
-PID5=$!
-echo $PID5
 
 wait
