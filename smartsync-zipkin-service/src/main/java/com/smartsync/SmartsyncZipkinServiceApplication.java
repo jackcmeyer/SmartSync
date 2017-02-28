ecc051540653f5d65d65f145a4573737cef12aac
Jack  Meyer <jackmeyerdev@gmail.com>
2017-02-28 16:35:52 -0600

    Working configuration services including config-service, zipking-service, eureka-service, and hystrix-dashboard

--- smartsync-zipkin-service/src/main/java/com/smartsync/SmartsyncZipkinServiceApplication.java
+++ smartsync-zipkin-service/src/main/java/com/smartsync/SmartsyncZipkinServiceApplication.java
@@ -2,8 +2,12 @@ package com.smartsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
 public class SmartsyncZipkinServiceApplication {
 
 	public static void main(String[] args) {
