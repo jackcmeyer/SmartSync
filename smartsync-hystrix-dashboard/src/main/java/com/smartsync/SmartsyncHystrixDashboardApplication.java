eb6658187430091123bd9478f9fe866cb2d35735
Jack  Meyer <jackmeyerdev@gmail.com>
2017-02-28 16:42:19 -0600

    Working configuration services including config-service, zipkin-service, hystrix-dashboard, and eureka service

--- smartsync-hystrix-dashboard/src/main/java/com/smartsync/SmartsyncHystrixDashboardApplication.java
+++ smartsync-hystrix-dashboard/src/main/java/com/smartsync/SmartsyncHystrixDashboardApplication.java
@@ -2,8 +2,12 @@ package com.smartsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
 public class SmartsyncHystrixDashboardApplication {
 
 	public static void main(String[] args) {
