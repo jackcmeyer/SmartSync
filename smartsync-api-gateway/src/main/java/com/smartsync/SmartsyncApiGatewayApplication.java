e77d921da2e2219fa713daad87636af48ce682f6
Jack  Meyer <jackmeyerdev@gmail.com>
2017-02-28 16:49:09 -0600

    Api Gateway Settings

--- smartsync-api-gateway/src/main/java/com/smartsync/SmartsyncApiGatewayApplication.java
+++ smartsync-api-gateway/src/main/java/com/smartsync/SmartsyncApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.smartsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SmartsyncApiGatewayApplication {
 
 	public static void main(String[] args) {
