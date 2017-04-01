22175850df0f3b540c778343b87c465221279740
Jack  Meyer <jackmeyerdev@gmail.com>
2017-04-01 14:24:38 -0500

    cors filtering for to do list service

---
+++ smartsync-todo-list-service/src/main/java/com/smartsync/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.smartsync.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * Class which handles CORS configurations.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "DELETE", "PUT")
+                .allowCredentials(false).maxAge(3600);
+    }
+
+}
