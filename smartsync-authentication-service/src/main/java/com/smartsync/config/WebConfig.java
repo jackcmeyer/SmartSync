6482f2b99a95e76302ffd23be2beab21320b9361
trevor-henderson <trevorsgit@gmail.com>
2017-04-12 17:57:07 -0500

    added auth service to project

---
+++ smartsync-authentication-service/src/main/java/com/smartsync/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.smartsync.config;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by trev on 4/3/17.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "DELETE", "PUT")
+                .allowCredentials(false).maxAge(3600);
+    }
+
+}
---
+++ smartsync-authentication-service/src/main/java/com/smartsync/error/UserNotRegisteredException.java
@@ -0,0 +1,36 @@
+package com.smartsync.error;
+
+/**
+ * Created by trev on 4/2/17.
+ */
+public class UserNotRegisteredException extends RuntimeException {
+
+    private String url;
+
+    private String message;
+
+    public UserNotRegisteredException(String message, String url) {
+        this.message = message;
+        this.url = url;
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String getMessage() {
+            return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
